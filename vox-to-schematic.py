45544d62e874d45184bfd45664414277d0c8b1cd
golder55 <57981038+golder55@users.noreply.github.com>
2021-08-17 10:33:12 +0530

    added code to choose block closest to the color
    added code to auto pick block closest to the color from block.json (dictionary of blocks and the average RGBA value of the blocks)

--- vox-to-schematic.py
+++ vox-to-schematic.py
@@ -1,11 +1,14 @@
 from pyvox.parser import VoxParser
 from nbtschem import SchematicFile
-from colors import color
+import json
+import math
+import numpy as np
 
-INPUT_FILE = "hitmonchan_one.vox"
-OUTPUT_FILE = "model.schematic"
+INPUT_FILE = "model.vox"
+OUTPUT_FILE = "model.schem"
 
 colors = {}
+blockjson = json.load(open('block.json'))
 
 vox_file = VoxParser(INPUT_FILE).parse()
 
@@ -14,13 +17,9 @@ sf = SchematicFile(shape=(height, length, width),offsetY=10)
 
 for voxel in vox_file.models[0].voxels:
     x, y, z, vox_color = voxel
-    if vox_color not in colors:
-        r, g, b, a = vox_file.palette[voxel.c]
-        hex_code = '#%02x%02x%02x' % (r, g, b)
-        inverted = '#%02x%02x%02x' % (255-r, 255-g, 255-b)
-        print(f"found color: {color(hex_code, inverted, hex_code)} ({vox_color})")
-        colors[vox_color] = input("Enter block name: > ")
+    r,g,b,a = vox_file.palette[voxel.c]
+    closest = min(blockjson, key=lambda k: math.sqrt(sum((np.array((r,g,b,a)) - np.array(blockjson[k]))**2)))
+    sf.set_block((z,x,y),closest)
+    print(x,y,z)
 
-    sf.set_block((z,x,y),colors[vox_color])
-print(colors)
 sf.save(OUTPUT_FILE)
